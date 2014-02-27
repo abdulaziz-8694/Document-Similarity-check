886fa737cb920c0c4e98fe870da73d068e6c7e1b
abdulaziz-8694 <cs12b1001@iith.ac.in>
2014-02-27 17:01:14 +0530

    Update similarity.py

--- similarity.py
+++ similarity.py
@@ -1,17 +1,17 @@
-##################################################
-#Filename: similarity.py
-#Author: Abdul Aziz 
-#How to run: On terminal type python similarity.py
-##################################################
-import Tkinter, tkFileDialog
-import os, mimetypes, sys
+########################################################################################################
+#Filename: similarity.py                                                                       #
+#Author: Abdul Aziz CS12B1001                                                                			#
+#How to run:  import the required library then run the console and type  python similarity.py  #
+#Description :this file takes input  and generates data.json and files									#
+#########################################################################################################
+import Tkinter, tkFileDialog, tkSimpleDialog #for gui
+import os, mimetypes, sys						#for 
 from munkres import Munkres
+import json, shutil, re
 
 S = []
-alpha = 0.4
-beta = 0.1
 
-#dirname = "C:/Users/AbdulAziz/Desktop/rs agarwal"
+dirname = "C:/Users/AbdulAziz/Desktop/rs agarwal"
 def init_matrix(n):
 	A = []
 	for i in range(n):
@@ -19,12 +19,11 @@ def init_matrix(n):
 		for j in range(n):
 			temp.append(0)
 		A.append(temp)
-	#print A
-	return A
-	
-	
+	return (A)
 def senSim(senA,senB,alpha):
+	senA = re.sub('[\W]', ' ', senA)
 	senA = senA.split(" ")
+	senB = re.sub('[\W]', ' ', senB)
 	senB = senB.split(" ")
 	A_union_B = len(set(senA).union(set(senB)))
 	A_intersection_B = len(set(senA).intersection(set(senB)))
@@ -34,17 +33,14 @@ def senSim(senA,senB,alpha):
 	return result
 
 def getFileSim(fileA,fileB,alpha,beta):
-	File1=open(fileA, "r")
-	File2=open(fileB, "r")
+	File1=open(dirname+'/'+fileA, "r")
+	File2=open(dirname+'/'+fileB, "r")
 	content1=File1.readlines()
-	#print content1
+	flag=0
 	content2=File2.readlines()
-	#print content2
 	if len(content1) > len(content2):
-		#print "big"
+		flag=1
 		content1,content2 = content2,content1
-	#print content1
-	#print content2
 	sim=[]
 	for sen_A in content1:
 		temp = []
@@ -53,63 +49,121 @@ def getFileSim(fileA,fileB,alpha,beta):
 			sen_B = sen_B.lower()
 			temp.append(senSim(sen_A,sen_B,alpha))
 		sim.append(temp)
-		
 	for i in range(len(sim)): #to make it a maximum cost problem
 		for j in range(len(sim[i])):
-			sim[i][j] = 1-sim[i][j]
+			sim[i][j] = 1.0-sim[i][j]
 	m=Munkres()
 	result_matrix=m.compute(sim)
-	#print sim
-	#print result_matrix
 	maxSimMatrix = []
 	for row,column in result_matrix:
 		if	sim[row][column]!=1.0:
-			maxSimMatrix.append(1-sim[row][column])
+			if flag==1:
+				row,column=column,row
+			maxSimMatrix.append([row,column])
 	FileSim=float(len(maxSimMatrix))/(len(content1))
 	
 	if FileSim<beta:
-		FileSim = 0;
-	
-	return FileSim
+		FileSim = 0
+	return (FileSim, maxSimMatrix)
 		
 	
 
 
-def getFilepaths(dirname):
-	filepaths=[]
-	#i=0
+def getFile(dirname):
+	files=[]
 	for file in os.listdir(dirname):
-		#print file
 		filepath = dirname+'/'+file
 		if mimetypes.guess_type(filepath) == ('text/plain',None ):
-			filepaths.append(dirname+'/'+file)
-			#print "text"
-		#else:
-			#print "not text"
-		#print filepath
-	return filepaths
-	
+			files.append(file)
+	return files
 
 root = Tkinter.Tk()
 root.withdraw()
 dirname = tkFileDialog.askdirectory(parent=root,initialdir="/",title='Please select a directory')
+alpha = tkSimpleDialog.askfloat('Alpha', 'Enter the value of alpha', initialvalue=0.6, minvalue=0.0, maxvalue=1.0)
+beta = tkSimpleDialog.askfloat('Beta', 'Enter the value of beta', initialvalue=0.1, minvalue=0.0, maxvalue=1.0)
+if dirname=='':
+	dirname='.'
+if alpha==None:
+	alpha=0.6
+if beta==None:
+	beta=0.1
 
-#if len(dirname ) > 0:
-   	#print "You chose %s" % dirname
 	
-files=getFilepaths(dirname)
-#print files
+files=getFile(dirname)
 S = init_matrix(len(files))
-
+sim=init_matrix(len(files))
+result=[]
 for a in range(len(files)):
+	temp1=[]
+	temp2=[]
 	for b in range(len(files)):
-		#print files[a]
 		if a==b:
 			S[a][b] = 1
 		else:
-			#print files[b]
-			S[a][b] = getFileSim(files[a],files[b],alpha,beta)
-print S
+			(x,y) = getFileSim(files[a],files[b],alpha,beta)
+			#print "just after", y
+			S[a][b]=x
+			sim[a][b]=y
+print sim
+#for creating json file
+def getData(dirname,files,S,sim,beta):
+	data={}
+	filelist = []
+	#print len(files)
+	for file in files:
+		temp={}
+		temp['name'] = file
+		content=open(dirname+'/'+file).read()
+		filelist.append(temp)
+	data['nodes'] = filelist
+	links = []
+	for i in range(len(S)):
+		for j in range(i+1,len(S)):
+			#print i,j
+			if	S[i][j]>=beta:
+				temp={}
+				temp['source'] = i
+				temp['target'] = j
+				temp['weight'] = S[i][j]
+				temp2=[]
+				temp['pairs'] = sim[i][j]
+				links.append(temp)
+	data['links'] = links
+	return data
+
+def getData2(dirname,files):
+	data=""
+	filesdata="["
+	name="["
+	for file in files:
+		name+="'"+file+"',"
+		f=open(dirname+'/'+file, "r")
+		content=f.readlines()
+		filesdata+="["
+		for sen in content:
+			l=str(sen)
+			m=l.replace('"',r'\"')
+			m=m.replace('<', r'&lt;')
+			m=m.replace('>', r'&gt;')
+			filesdata+="\""+m+"\","
+		filesdata+="],"
+	name+="];"
+	filesdata+="]"
+	filesdata=re.sub("\n","",filesdata)
+	data = "filename="+name+'\n'+"filedata="+filesdata
+	return data
+
+def dumpData(data,dirname):
+	with open('data.json', 'wb') as f:
+		json.dump(data, f)
+		
+data=getData(dirname,files,S,sim,beta)
+data2 = getData2(dirname,files)
+filedata=open("data.js", "w")
+filedata.write(data2)
+dumpData(data,dirname)
+
 
 
 
